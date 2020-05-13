ba135d7852f26a8aa182694e25eb1c232ffb23ca
psj03 <paul.johnson2@sky.uk>
2020-05-13 14:25:13 +0100

    Updates

---
+++ precache-manifest.90de244967bb1c1f387da55ce22c2936.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b764ef6a2dfda6824dadf303faf6bfb2",
+    "url": "/challengeaccepteduk.github.io/index.html"
+  },
+  {
+    "revision": "8d85908d5b5506b44c73",
+    "url": "/challengeaccepteduk.github.io/static/css/2.1f3ce829.chunk.css"
+  },
+  {
+    "revision": "18be8519c78586e393b0",
+    "url": "/challengeaccepteduk.github.io/static/css/main.f9add0bd.chunk.css"
+  },
+  {
+    "revision": "8d85908d5b5506b44c73",
+    "url": "/challengeaccepteduk.github.io/static/js/2.8119487b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/challengeaccepteduk.github.io/static/js/2.8119487b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18be8519c78586e393b0",
+    "url": "/challengeaccepteduk.github.io/static/js/main.1a8374f4.chunk.js"
+  },
+  {
+    "revision": "b45cdc9e62518574a43c",
+    "url": "/challengeaccepteduk.github.io/static/js/runtime-main.4f9d6e7f.js"
+  },
+  {
+    "revision": "57e736a7763587f8d7f4f701b505c1dd",
+    "url": "/challengeaccepteduk.github.io/static/media/Lightbulb.57e736a7.png"
+  },
+  {
+    "revision": "43cb1409bc3b81ea10e66c67e4ee2b03",
+    "url": "/challengeaccepteduk.github.io/static/media/Paul.43cb1409.png"
+  },
+  {
+    "revision": "1c69bb4f3302d75e2ba7fb6e382524ce",
+    "url": "/challengeaccepteduk.github.io/static/media/Steph.1c69bb4f.png"
+  },
+  {
+    "revision": "efaa51cbf44d3a31eb68e213c9012543",
+    "url": "/challengeaccepteduk.github.io/static/media/aboutflow.efaa51cb.png"
+  },
+  {
+    "revision": "d405f32b6c9167b9b27232935aab074b",
+    "url": "/challengeaccepteduk.github.io/static/media/home-appscreen-1.d405f32b.png"
+  },
+  {
+    "revision": "fd35751084d4d8d1a9b2f0ff7587af32",
+    "url": "/challengeaccepteduk.github.io/static/media/home-appscreen-2.fd357510.png"
+  },
+  {
+    "revision": "7d78fe4bce84ff5790e0620434d43349",
+    "url": "/challengeaccepteduk.github.io/static/media/home-appscreen-3.7d78fe4b.png"
+  },
+  {
+    "revision": "105306db714f7ec56dec215753f11f79",
+    "url": "/challengeaccepteduk.github.io/static/media/home-appscreen-4.105306db.png"
+  },
+  {
+    "revision": "ff22b0ab486a1c2ddcd64d50275fddd6",
+    "url": "/challengeaccepteduk.github.io/static/media/mainlogo.ff22b0ab.png"
+  },
+  {
+    "revision": "8044c9e9512a4402b04587c2c1f0c834",
+    "url": "/challengeaccepteduk.github.io/static/media/notification.8044c9e9.png"
+  },
+  {
+    "revision": "ea03a719187b2b8503c09d68e0f6e814",
+    "url": "/challengeaccepteduk.github.io/static/media/pencil.ea03a719.png"
+  },
+  {
+    "revision": "6bb883bba4b9bd843a361ebe888ad0f1",
+    "url": "/challengeaccepteduk.github.io/static/media/search.6bb883bb.png"
+  },
+  {
+    "revision": "03c831f7b9cdf705e0565484d568b6f6",
+    "url": "/challengeaccepteduk.github.io/static/media/ubuntu-v14-latin-regular.03c831f7.eot"
+  },
+  {
+    "revision": "1e926e228a9e2e1e77034f624211e2b4",
+    "url": "/challengeaccepteduk.github.io/static/media/ubuntu-v14-latin-regular.1e926e22.woff"
+  },
+  {
+    "revision": "3c51cf95e0cf5b2145510b36882a66ed",
+    "url": "/challengeaccepteduk.github.io/static/media/ubuntu-v14-latin-regular.3c51cf95.svg"
+  },
+  {
+    "revision": "5b23eeb3a32b30e91682d601535d2a89",
+    "url": "/challengeaccepteduk.github.io/static/media/ubuntu-v14-latin-regular.5b23eeb3.woff2"
+  },
+  {
+    "revision": "694f32f4f139e578f8303309a343cfa0",
+    "url": "/challengeaccepteduk.github.io/static/media/ubuntu-v14-latin-regular.694f32f4.ttf"
+  }
+]);
