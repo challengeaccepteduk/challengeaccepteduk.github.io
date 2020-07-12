d8c15220064677401f1f1683e163f3d046af6820
mattjburrows <mattjburrows@gmail.com>
2020-07-12 18:02:41 +0100

    adds sticky iphone to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
-import Content from '../components/Content';
+import { container, image, content, text } from './Index.module.css';
 
 const Home = () => {
   return (
-    <Content>
-      <div>
-        <h3>Discover challenges</h3>
-        <p>Discover challenges in the things you love from sport, travel, health & fitness, food and more. Or create your own challenge.</p>
+    <section className={container}>
+      <div className={content}>
+        <div className={text}>
+          <h3>Discover challenges</h3>
+          <p>Discover challenges in the things you love from sport, travel, health & fitness, food and more. Or create your own challenge.</p>
+        </div>
+        <div className={text}>
+          <h3>Set reminders</h3>
+          <p>Set notification reminders for your challenges based around your schedule.</p>
+        </div>
+        <div className={text}>
+          <h3>Challenge friends</h3>
+          <p>Add friends so you can share your progress and see what challenges each other are completing.</p>
+        </div>
+        <div className={text}>
+          <h3>Create your own challenges</h3>
+          <p>Create your own challenge from scratch based on your goals.</p>
+        </div>
       </div>
-      <div>
-        <h3>Set reminders</h3>
-        <p>Set notification reminders for your challenges based around your schedule.</p>
-      </div>
-      <div>
-        <h3>Challenge friends</h3>
-        <p>Add friends so you can share your progress and see what challenges each other are completing.</p>
-      </div>
-      <div>
-        <h3>Create your own challenges</h3>
-        <p>Create your own challenge from scratch based on your goals.</p>
-      </div>
-    </Content>
+      <figure className={image}>
+        <img src="/Home/iphone-sample-yes-01_2x.png" alt="" />
+      </figure>
+    </section>
   )
 }
 
