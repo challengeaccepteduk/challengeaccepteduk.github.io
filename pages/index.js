9856d0b95245f959c13e64fea0e8da2335ff041a
mattjburrows <mattjburrows@gmail.com>
2020-07-12 18:31:47 +0100

    update site background colour, fix spacing on the navigation

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import { container, image, content, text } from './Index.module.css';
+import { container, image, content, feature, heading, text } from './Index.module.css';
 
 const Home = () => {
   return (
     <section className={container}>
       <div className={content}>
-        <div className={text}>
-          <h3>Discover challenges</h3>
-          <p>Discover challenges in the things you love from sport, travel, health & fitness, food and more. Or create your own challenge.</p>
+        <div className={feature}>
+          <h3 className={heading}>Discover challenges 👀</h3>
+          <p className={text}>Discover challenges in the things you love from sport, travel, health & fitness, food and more. Or create your own challenge.</p>
         </div>
-        <div className={text}>
-          <h3>Set reminders</h3>
-          <p>Set notification reminders for your challenges based around your schedule.</p>
+        <div className={feature}>
+          <h3 className={heading}>Set reminders ⏰</h3>
+          <p className={text}>Set notification reminders for your challenges based around your schedule.</p>
         </div>
-        <div className={text}>
-          <h3>Challenge friends</h3>
-          <p>Add friends so you can share your progress and see what challenges each other are completing.</p>
+        <div className={feature}>
+          <h3 className={heading}>Challenge friends 🙋‍</h3>
+          <p className={text}>Add friends so you can share your progress and see what challenges each other are completing.</p>
         </div>
-        <div className={text}>
-          <h3>Create your own challenges</h3>
-          <p>Create your own challenge from scratch based on your goals.</p>
+        <div className={feature}>
+          <h3 className={heading}>Create your own challenges 📝</h3>
+          <p className={text}>Create your own challenge from scratch based on your goals.</p>
         </div>
       </div>
       <figure className={image}>
