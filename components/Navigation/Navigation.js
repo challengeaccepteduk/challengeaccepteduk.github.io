1cadacc4136185768960b103e62b69e8a4c811f5
mattjburrows <mattjburrows@gmail.com>
2020-07-28 21:54:28 +0100

    add mobile navigation in

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,35 +1,43 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
 
-import { container, logo, navigation, link } from './Navigation.module.css';
+import { container, toggle, logo, showNavigation, hideNavigation, item, link } from './Navigation.module.css';
 
-const Navigation = () => (
-  <nav>
-    <div className={container}>
-      <div className={logo}><img src="/logo-192-transparent.png" alt="" /></div>
-      <ul className={navigation}>
-        <li>
-          <Link href="/">
-            <a className={link}>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about-us">
-            <a className={link}>About us</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/partner-with-us">
-            <a className={link}>Partner with us</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact-us">
-            <a className={link}>Contact us</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+const Navigation = () => {
+  const [navigationVisibility, setNavigationVisibility] = useState(false);
+  const toggleNavigationVisibility = () => setNavigationVisibility(!navigationVisibility);
+
+  return (
+    <nav>
+      <div className={container}>
+        <div className={logo}><img src="/logo-192-transparent.png" alt="" /></div>
+        <button className={toggle} onClick={toggleNavigationVisibility}>{navigationVisibility ? 'Close Navigation' : 'Open Navigation'}</button>
+        <ul className={navigationVisibility ? showNavigation : hideNavigation}>
+          <li className={item}>
+            <Link href="/">
+              <a className={link}>Home</a>
+            </Link>
+          </li>
+          <li className={item}>
+            <Link href="/about-us">
+              <a className={link}>About us</a>
+            </Link>
+          </li>
+          <li className={item}>
+            <Link href="/partner-with-us">
+              <a className={link}>Partner with us</a>
+            </Link>
+          </li>
+          <li className={item}>
+            <Link href="/contact-us">
+              <a className={link}>Contact us</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+};
 
 export default Navigation;
