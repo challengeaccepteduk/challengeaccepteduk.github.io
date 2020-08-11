65560f9cab67f72b3a7e92bedefb4bf8587fccbc
psj03 <paul.johnson2@sky.uk>
2020-08-11 12:40:17 +0100

    [release] challenge and friend redirect to home

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/challenge/:id',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/challenge/:id/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/friend/:id',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/friend/:id/',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   exportTrailingSlash: true,
   exportPathMap: function() {
     return {
