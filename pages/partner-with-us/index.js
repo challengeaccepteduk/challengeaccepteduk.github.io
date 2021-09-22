03e7ac2287f49c273d465499f90440907cf52351
stephmandeville <steph.mandeville@live.com>
2021-09-22 10:52:48 +0100

    Changed partner page to .md file

--- pages/partner-with-us/index.js
+++ pages/partner-with-us/index.js
@@ -1,46 +1,25 @@
+import { Parser } from 'html-to-react';
+
+import markdownToHtml from '../../lib/markdown-to-html';
 import Content from '../../components/Content';
 
-const PartnerWithUs = () => {
+const PartnerWithUs = ({ pageContent }) => {
+  const parser = new Parser();
   return (
-    <Content>
-      <h1>Challenge Accepted creates fun and engaging challenges for people to take part in from health and fitness to travel, theatre and more. We help thousands of people stay motivated with what they start.</h1>
-      <h2>Coming soon - marketing campaigns on Challenge Accepted</h2>
-      <p>We can work with you to create an engaging branded challenge on the app that's focused on your customers having fun and taking on a challenge for something you are both passionate about.
-      <br/><br/> 🚀 Engage your audience with a bespoke challenge on the app
-      <br/> 🏷️ Help people complete their challenge with an offer for your brand
-      <br/> 🎯 Target people based on their interests, not demographics
-      <br/><br/> To find out more about branded challenges and to discuss how we can support your marketing campaign, please <a href="/contact-us">get in touch.</a></p>
-      <h2>Supporting UK charities</h2>
-      <p>Since the launch of Challenge Accepted in July last year, we've had over 2,000 people participate in virtual fundraising challenges on the app from UK charities including:</p>
-
-      <div class="scrollable-table-outer wrapper">
-      <table class="scrollable-table-inner">
-      <tbody>
-        <tr>
-            <td><img src="/Partners/bhf-logo.png" alt="British Heart Foundation" /></td>
-            <td><img src="/Partners/tct-logo.png" alt="Teenage Cancer Trust" /></td>
-        </tr>
-        <tr>
-            <td><img src="/Partners/su2c-logo.png" alt="Stand Up To Cancer" /></td>
-            <td><img src="/Partners/shine-logo.png" alt="Shine Cancer Support" /></td>
-        </tr>
-        </tbody>
-      </table>
-      </div>
-
-      <p>We can support UK charities by hosting fundraising events on the app to help participants keep track and stay motivated throughout the campaign.
-      <br/><br/>If you are a registered UK charity please <a href="/contact-us">get in touch</a> with us to see how we can support your virtual fundraising challenges. </p>
-    </Content>
+    <Content>{parser.parse(pageContent)}</Content>
   );
 };
 
 export async function getStaticProps(context) {
+  const pageContent = await markdownToHtml('./partner-with-us/partner-with-us.md');
+
   return {
     props: {
       seoTitle: 'Partner with us',
       seoDescription: 'Creating fun and interactive challenges',
       headline: 'Creating fun and interactive challenges',
       includeRegisterButton: false,
+      pageContent,
     },
   };
 }
--- pages/partner-with-us/partner-with-us.md
+++ pages/partner-with-us/partner-with-us.md
@@ -1,6 +1,8 @@
 
 ## Challenge Accepted creates fun and engaging challenges for people to take part in from health and fitness to travel, theatre and more. We help thousands of people stay motivated with what they start.</h1>
-Coming soon - marketing campaigns on Challenge Accepted
+
+## Coming soon - marketing campaigns on Challenge Accepted
+
 We can work with you to create an engaging branded challenge on the app that's focused on your customers having fun and taking on a challenge for something you are both passionate about.
 
 🚀 Engage your audience with a bespoke challenge on the app
@@ -13,13 +15,14 @@ To find out more about branded challenges and to discuss how we can support your
 
 ## Supporting UK charities
 
-Since the launch of Challenge Accepted in July last year, we've had over 2,000 people participate in virtual fundraising challenges on the app from UK charities including:
+In the last 12 months, we've had over 2,000 people participate in virtual fundraising challenges on the Challenge Accepted app.
 
-| Partners  |  |
-| ------------- |:-------------:|
-| ![This is a alt text.](./Partners/bhf-logo.png" This is a sample image.")  | ![This is a alt text.](./Partners/tct-logo.png" This is a sample image.") |
-| ![This is a alt text.](./Partners/su2c-logo.png" This is a sample image.")  | ![This is a alt text.](.Partners/shine-logo.png" This is a sample image.")  |
-
+|Charities supported|
+| ------------- |:-------------:|:-------------:|
+| ![test](/public/Partners/bhf-logo.png "This is a sample image.")  | ![test](/Partners/tct-logo.png "This is a sample image.") | 
+| ![test](/public/Partners/su2c-logo.png "This is a sample image.")  | ![test](/public/Partners/shine-logo.png "This is a sample image.")  |
+| ![test](/public/Partners/sam-logo.png "This is a sample image.") | ![test](/public/Partners/suppdogs-logo.png "This is a sample image.") |
 
 We can support UK charities by hosting fundraising events on the app to help participants keep track and stay motivated throughout the campaign.
+
 If you are a registered UK charity please [get in touch](/pages/contact-us) with us to see how we can support your virtual fundraising challenges.
